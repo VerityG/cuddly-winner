0c86eae8208880823acad9f1c8d079db84181e6a
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-04-27 23:39:15 +0300

    Второе задание

--- homework1/zad2.py
+++ homework1/zad2.py
@@ -1,11 +1,10 @@
-print('Введите длину списка(x > 2): ')
-x = int(input())
+x = int(input('Введите размер списка(x > 2): '))
 while x <= 2:
     print('Неверное значение')
     x = int(input('Попробуйте еще раз: '))
     continue
 y = []
 for i in range(x):
-    y.append(int(input()))
+    y.append(int(input('Введите значение: ')))
 y.sort()
 print('Максимальные числа в вашем списке:', y[:-2:-1], 'и', y[-2:-3:-1])
