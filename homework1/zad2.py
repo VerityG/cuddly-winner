a6907bffe7156971dd8530d71079a18f1b17cd8d
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-04-27 23:07:03 +0300

    Второе задание

--- homework1/zad2.py
+++ homework1/zad2.py
@@ -1,5 +1,9 @@
 print('Введите длину списка(x > 2): ')
 x = int(input())
+while x <= 2:
+    print('Неверное значение')
+    x = int(input('Попробуйте еще раз: '))
+    continue
 y = []
 for i in range(x):
     y.append(int(input()))
