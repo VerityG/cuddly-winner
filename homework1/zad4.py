08fd187ffb8de1e698493e82f58dcdd30e144203
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-05-01 18:32:57 +0300

    Четвертое задание второй вариант

--- homework1/zad4.py
+++ homework1/zad4.py
@@ -1,11 +1,17 @@
-import math
-x = float(input('Введите x cos: '))
+def fac(n):
+    a = 1
+    n = n * 2
+    for i in range(1, n + 1): # Мне кажется что у меня какая то дичь в функции написана
+        a *= i
+    return a
+
+
+x = int(input('Введите x cos: '))
 n = int(input('Введите N: '))
 y = []
 for i in range(n):
-    cos = ((-1) ** i) * (x ** (2 * i)) / (math.factorial(2 * i))
+    cos = ((-1) ** i) * (x ** (2 * i)) / (fac(i))
     y.append(cos)
-
 
 y = sum(y)
 print(y)
