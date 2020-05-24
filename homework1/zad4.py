7d6b6f1d234dae2a1eeb006a6b3e76eecf6df47a
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-05-24 16:18:23 +0300

    Задание 4

--- homework1/zad4.py
+++ homework1/zad4.py
@@ -1,7 +1,6 @@
 def fac(n):
     a = 1
-    n = n * 2  # стоило не здесь умножать n на 2, а в цикле вызывать fac(2 * i)
-    for i in range(1, n + 1): # Мне кажется что у меня какая то дичь в функции написана
+    for i in range(1, n + 1):
         a *= i
     return a
 
@@ -10,7 +9,7 @@ x = int(input('Введите x cos: '))
 n = int(input('Введите N: '))
 y = []
 for i in range(n):
-    cos = ((-1) ** i) * (x ** (2 * i)) / (fac(i))
+    cos = ((-1) ** i) * (x ** (2 * i)) / (fac(2 * i))
     y.append(cos)
 
 y = sum(y)
