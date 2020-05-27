6d6469fb2a1eca1d1546f78467d44fbd42933523
Ivan Vasiliuk <ivan.vasiliuk@gmail.com>
2020-05-27 03:23:59 +0300

    Zad 1

---
+++ homework4/zad1d.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+
+def multiply(fn):
+    def wrapper(*numbers):
+        result = reduce(lambda a, b: a * b, numbers)
+        fn(result)
+    return wrapper
+
+
+@multiply
+def func(x):
+    print(x)
+
+
+numbers = map(int, input().split())
+func(*numbers)
